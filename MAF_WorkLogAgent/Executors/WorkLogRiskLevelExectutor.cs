ce98df698873d0029df4f921a9991a74c2c447e6
yuhandbcontext <yuhandbcontext@noreply.gitcode.com>
2025-10-28 23:22:05 +0800

    加入第二个流程步骤

--- MAF_WorkLogAgent/Executors/WorkLogRiskLevelExectutor.cs
+++ MAF_WorkLogAgent/Executors/WorkLogRiskLevelExectutor.cs
@@ -6,18 +6,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MAF_WorkLogAgent.Executors
 {
     internal class WorkLogRiskLevelExectutor : Executor<WorkLogTypeDto>
     {
-        public WorkLogRiskLevelExectutor(AIAgent riskAgent) : base("SendEmailExecutor") { }
+        private AIAgent _riskAgent;
+        public WorkLogRiskLevelExectutor(AIAgent riskAgent) : base("SendEmailExecutor")
+        {
+            this._riskAgent = riskAgent;
+        }
 
         public override async ValueTask HandleAsync(WorkLogTypeDto message, IWorkflowContext context, CancellationToken cancellationToken = default)
         {
             WorkLogModel model = await context.ReadStateAsync<WorkLogModel>(message.Id, scopeName: "myscope");
-            await context.YieldOutputAsync($"日志内容：{model.Content} 分类结果: {message.Type}");
+            var response = await this._riskAgent.RunAsync(model.Content);
+            string restext = response.Text.Replace("```json", "").Replace("```", "");
+            var detectionResult = JsonSerializer.Deserialize<WorkLogRiskDto>(restext);
+            await context.YieldOutputAsync($"日志内容：{model.Content} 分类结果: {message.Type} 风险等级：{detectionResult.Risk_Level}");
         }
 
     }
