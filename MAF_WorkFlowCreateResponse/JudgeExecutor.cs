154cfe80d51f51bc3123dba19f3117deffd0f106
yuhandbcontext <yuhandbcontext@noreply.gitcode.com>
2025-11-01 20:35:47 +0800

    RequestPort

---
+++ MAF_WorkFlowCreateResponse/JudgeExecutor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Agents.AI.Workflows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAF_WorkFlowCreateResponse
+{
+    /// <summary>
+    /// 输入值 和 目标值对比
+    /// </summary>
+    public class JudgeExecutor() : Executor<int>("Judge")
+    {
+        private int _targetnum;
+        public JudgeExecutor(int targetnum) : this()
+        {
+            _targetnum = targetnum;
+        }
+        public override async ValueTask HandleAsync(int message, IWorkflowContext context, CancellationToken cancellationToken = default)
+        {
+            if (_targetnum != message)
+            {
+                await context.YieldOutputAsync("不相等");
+            }
+            else
+            {
+                await context.YieldOutputAsync("相等");
+            }
+        }
+    }
+}
---
+++ MAF_WorkFlowCreateResponse/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Agents.AI.Workflows;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+using System.ClientModel;
+
+namespace MAF_WorkFlowCreateResponse
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+             .AddUserSecrets<Program>()
+             .Build();
+
+            var apikey = config["apikey"];
+            // Console.WriteLine($"Secret: {apikey}");
+
+            IChatClient chatClient = new OpenAIClient(new ApiKeyCredential(apikey),
+            new OpenAIClientOptions()
+            {
+                Endpoint = new Uri("https://api.siliconflow.cn/v1/"),
+
+            })
+            .GetChatClient("Qwen/Qwen2.5-7B-Instruct").AsIChatClient();
+            //输入CmdEnum  输出int  （触发事件）
+            RequestPort numberRequestPort = RequestPort.Create<CmdEnum, int>("numberInput");
+
+            JudgeExecutor judgeExecutor = new(42);
+
+            Workflow workflow = new WorkflowBuilder(numberRequestPort)
+               .AddEdge(numberRequestPort, judgeExecutor)
+               .WithOutputFrom(judgeExecutor)
+               .Build();
+            await using StreamingRun handle = await InProcessExecution.StreamAsync(workflow, CmdEnum.INPUT);//传入枚举类型，事件里面输入
+            await foreach (WorkflowEvent evt in handle.WatchStreamAsync())
+            {
+                switch (evt)
+                {
+                    case RequestInfoEvent requestInputEvt:
+                        if (requestInputEvt.Request.DataIs<CmdEnum>())
+                        {
+                            CmdEnum cmdEnum = requestInputEvt.Request.DataAs<CmdEnum>();
+                            if (cmdEnum == CmdEnum.INPUT)
+                            {
+                                string inputstr = Console.ReadLine();
+                                ExternalResponse response = requestInputEvt.Request.CreateResponse(Convert.ToInt32(inputstr));
+                                await handle.SendResponseAsync(response); //RequestPort的返回值 传递到下一个节点
+                            }
+                        }
+                        //ExternalResponse response = HandleExternalRequest(requestInputEvt.Request);
+                        //await handle.SendResponseAsync(response);
+                        break;
+
+                    case WorkflowOutputEvent outputEvt:
+                        // The workflow has yielded output
+                        Console.WriteLine($"流程执行完毕，返回结果: {outputEvt.Data}");
+                        return;
+                }
+            }
+            Console.WriteLine("运行完成");
+        }
+    }
+}
